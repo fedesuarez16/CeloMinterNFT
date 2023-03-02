cdfdf290fa32a8284331f0eb58848fa999dac8e0
federico suarez <fedesuarez16@gmail.com>
2023-03-02 01:54:07 -0300

    tests

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -6,20 +6,39 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
 
 contract MyNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
+    PaymentSplitter private _paymentSplitter;
 
-    constructor() ERC721("MyNFT", "MNFT") {}
-
-    function safeMint(address to, string memory uri) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+    // constructor(address[] memory payees, uint256[] memory shares) ERC721("MyNFT", "MNFT") {
+    //     _paymentSplitter = new PaymentSplitter(payees, shares);
+    // }
+ 
+    constructor(address[] memory payees, uint256[] memory shares) ERC721("MyNFT", "MNFT") {
+        _paymentSplitter = new PaymentSplitter(payees, shares);
     }
+
+
+
+
+    function safeMint(address to, string memory uri) public payable {
+    require(msg.value == 1000000000000000000  , "Payment amount is incorrect.");
+    uint256 tokenId = _tokenIdCounter.current();
+    _tokenIdCounter.increment();
+    _safeMint(to, tokenId);
+    _setTokenURI(tokenId, uri);
+    payable(owner()).transfer(msg.value);
+}
+
+function paymentSplitter() public view returns (address) {
+    return address(_paymentSplitter);
+}
+
+
 
     // The following functions are overrides required by Solidity.
 
--- test/nft-test.js
+++ test/nft-test.js
@@ -1,47 +1,31 @@
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
 
 describe("MyNFT", function () {
-  this.timeout(50000);
-
+  let MyNFT;
   let myNFT;
   let owner;
-  let acc1;
-  let acc2;
+  let addr1;
+  let addr2;
+  const uri = "https://example.com/token";
 
-  this.beforeEach(async function () {
-    // This is executed before each test
-    // Deploying the smart contract
-    const MyNFT = await ethers.getContractFactory("MyNFT");
-    [owner, acc1, acc2] = await ethers.getSigners();
-
+  beforeEach(async function () {
+    MyNFT = await ethers.getContractFactory("MyNFT");
     myNFT = await MyNFT.deploy();
+    [owner, addr1, addr2] = await ethers.getSigners();
   });
 
-  it("Should set the right owner", async function () {
-    expect(await myNFT.owner()).to.equal(owner.address);
-  });
+  it("should mint a new NFT and transfer 0.1 ETH to the contract owner", async function () {
+    // Set the payment amount to 0.1 ETH
+    const paymentAmount = ethers.utils.parseEther("0.1");
 
-  it("Should mint one NFT", async function () {
-    expect(await myNFT.balanceOf(acc1.address)).to.equal(0);
+    // Call the safeMint function with the correct payment amount
+    await expect(() =>
+      myNFT.safeMint(addr1.address, uri, { value: paymentAmount })
+    ).to.changeEtherBalance(owner, paymentAmount);
 
-    const tokenURI = "https://example.com/1";
-    const tx = await myNFT.connect(owner).safeMint(acc1.address, tokenURI);
-    await tx.wait();
-
-    expect(await myNFT.balanceOf(acc1.address)).to.equal(1);
-  });
-
-  it("Should set the correct tokenURI", async function () {
-    const tokenURI_1 = "https://example.com/1";
-    const tokenURI_2 = "https://example.com/2";
-
-    const tx1 = await myNFT.connect(owner).safeMint(acc1.address, tokenURI_1);
-    await tx1.wait();
-    const tx2 = await myNFT.connect(owner).safeMint(acc2.address, tokenURI_2);
-    await tx2.wait();
-
-    expect(await myNFT.tokenURI(0)).to.equal(tokenURI_1);
-    expect(await myNFT.tokenURI(1)).to.equal(tokenURI_2);
+    // Check that the NFT was minted
+    expect(await myNFT.totalSupply()).to.equal(1);
+    expect(await myNFT.ownerOf(0)).to.equal(addr1.address);
+    expect(await myNFT.tokenURI(0)).to.equal(uri);
   });
 });
